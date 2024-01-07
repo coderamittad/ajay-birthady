fa7df3830be9b206a95604ac5552b67c119e4ddd
coderamittad <155918173+coderamittad@users.noreply.github.com>
2024-01-08 03:35:58 +0530

    Add files via upload

---
+++ next.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const birthdayWish = document.getElementById("birthday-wish");
+    setTimeout(function() {
+      birthdayWish.style.display = "block";
+    }, 1000); // Show the birthday wish after 1 second
+  });
+  
